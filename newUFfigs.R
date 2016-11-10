3f3c20fb5662f40ea760791b0d4027f3776d0ebf
Ginger Allington <gallington@rstudio01.research.sesync.org>
2016-11-10 13:30:19 -0500

    inital commit

--- newUFfigs.R
+++ newUFfigs.R
@@ -2,7 +2,11 @@
 #last edit 5/11 updating color lines and fixing subsetting issues
 #last edit 4/26 adding in Sukh plotting code
 
-#NOTE:  Must run "data.prep.R" AND "multiplot.R" files first, with files from "Mongolia_SDM/data"
+
+#CAUTION!! If import new runs make sure Hi/Med/Lo are labelled appropriately!
+#Old version of this code had levels labelled backwards!
+#NOTE:  Must run "data.prep.R" (AND "multiplot.R" if want to use that) files first, 
+#with files from "Mongolia_SDM/data"
 #which are exported model projections from Vensim
 
 setwd("/nfs/gallington-data/Mongolia_SDM/")
@@ -22,7 +26,7 @@ library(scales)
 #Need to subset the main dataset Xgl_All, which was created in teh data.prep.R sheet
 #will do it now in two steps
 
-#Subset Step 1: Baseline-Scen3:
+#Subset Step 1: Baseline-Scen3 @ UF= 75%:
 xBS3<- Xgl_All%>%
   filter(UF==0.75)%>%
     filter(Scenario%in%c("Baseline","Scen1","Scen2","Scen3"))
@@ -42,7 +46,7 @@ xFig<- rbind(xBS3, x6S4)
 #################################################
 #Just plotting the midrange UFs (=0.75) except for Scen 4 which is =0.6
 ##################################################
-#Grassland
+#Grassland ALL Scenarios Black & White with symbols
 xg<- ggplot(xFig, aes(x=Year, y=GrassArea, group=Scenario, shape=Scenario))+
   geom_line() + geom_point()+
   guides(override.aes = list(size=12))+
@@ -53,12 +57,14 @@ xg<- ggplot(xFig, aes(x=Year, y=GrassArea, group=Scenario, shape=Scenario))+
   theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
   scale_x_discrete(breaks=seq(1990,2050,5))+
   #theme(legend.position="none")+
+  theme(legend.text=element_text(labels=c("Base Model", "Increased Precip", "No Grass Policy", "No Crop Policy", "Worst Case")))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   labs(x="Year", y="Grassland area (ha)")
 xg
 
-xg+geom_line(data=xS46)+geom_point()  
-#color lines:
+xg+geom_line(data=xS46)+geom_point()
+
+#ALL Scenarios, color lines:
 xg.col<-ggplot(data=xFig, aes(x=Year, y=GrassArea, group=Scenario, shape=Scenario, color=Scenario)) +
   geom_line(size=1.2) + #geom_point(size=2)+
   guides(override.aes = list(size=12))+
@@ -72,6 +78,19 @@ xg.col<-ggplot(data=xFig, aes(x=Year, y=GrassArea, group=Scenario, shape=Scenari
   labs(x="Year", y="Grassland area (ha)")
 xg.col
 
+#Just Base Model w color line:
+xg.col.base<- ggplot(subset(xFig, Scenario=="Baseline"),aes(x=Year, y=GrassArea, group=Scenario))+
+  geom_line(size=1.2, color= "#fb6a4a") + 
+  #guides(override.aes = list(size=12))+
+  #scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Grassland area (ha)")
+  
 
 #Crop
 xc<- ggplot(xFig, aes(x=Year, y=CropArea, group=Scenario, shape=Scenario))+
@@ -101,6 +120,21 @@ xc.col<-ggplot(data=xFig, aes(x=Year, y=CropArea, group=Scenario,  color=Scenari
   labs(x="Year", y="Cropland area (ha)")
 xc.col
 
+#Just Basline w color line:
+xc.col.base<-ggplot(subset(xFig, Scenario=="Baseline"), aes(x=Year, y=CropArea, group=Scenario)) +
+  geom_line(size=1.2, color= "#fb6a4a")+
+  guides(override.aes = list(size=12))+
+  scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Cropland area (ha)")
+
+
+
 #Livestock
 xl<- ggplot(xFig, aes(x=Year, y=LskPop, group=Scenario, shape=Scenario))+
   geom_line() + geom_point()+
@@ -128,6 +162,20 @@ xl.col<-ggplot(data=xFig, aes(x=Year, y=LskPop, group=Scenario, shape=Scenario, 
   #theme(legend.position="none")+
   labs(x="Year", y="Livestock Population EOY (s.u.)")
 xl.col
+
+#Just Baseline Livestock w color line:
+xl.col.base<-ggplot(subset(xFig, Scenario=="Baseline"), aes(x=Year, y=LskPop, group=Scenario, shape=Scenario)) +
+  geom_line(size=1.2, color="#fb6a4a") +# geom_point(size=2)+ geom_jitter(width=1, height=2)+
+  guides(override.aes = list(size=12))+
+  scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Livestock Population EOY (s.u.)")
+xl.col.base
 
 #Biomass
 xr<- ggplot(xFig, aes(x=Year, y=Biomass, group=Scenario, shape=Scenario))+
@@ -157,10 +205,24 @@ xb.col<-ggplot(data=xFig, aes(x=Year, y=Biomass, group=Scenario, shape=Scenario,
   labs(x="Year", y="Remaining Biomass EOY (kg/ha)")
 xb.col
 
+#Just Baseline Biomass w color line:
+xb.col.base<-ggplot(subset(xFig, Scenario=="Baseline"), aes(x=Year, y=Biomass, group=Scenario, shape=Scenario)) +
+  geom_line(size=1.2, color="#fb6a4a") +# geom_point(size=2)+ geom_jitter(width=1, height=2)+
+  guides(override.aes = list(size=12))+
+  scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Remaining Biomass EOY (kg/ha)")
+xb.col.base
+
 #4/28 specified new file name to export these new figs w UF=0.6 for Scen4. They look diff so 
 #keeping old ones for comparison instead of overriding.
 #5/11 fixed data subsetting code to get all years and also added plotting color lines 
-setwd("./plots")
+setwd("/nfs/gallington-data/Mongolia_SDM/plots")
 
 ggsave(xg, file="Xg_grass.png", height=5, width=8,dpi=600)
 #ggsave(xg, file="Xg_grassColor.png", height=5, width=8,dpi=600)
@@ -174,6 +236,14 @@ ggsave(xg.col, file="Xg_grassColor.png", height=5, width=8,dpi=600)
 ggsave(xc.col, file="Xg_cropColor.png", height=5, width=8,dpi=600)
 ggsave(xl.col, file="Xg_lskColor.png", height=5, width=8,dpi=600)
 ggsave(xb.col, file="Xg_rmbColor.png", height=5, width=8,dpi=600)
+
+
+#SAVE BASELINE ONLY PLOTS:
+
+ggsave(xg.col.base, file="Xg_base_grass.png", height=5, width=8, dpi=600)
+ggsave(xl.col.base, file="Xg_base_grass.png", height=5, width=8, dpi=600)
+ggsave(xb.col.base, file="Xg_base_grass.png", height=5, width=8, dpi=600)
+
 
 ###################
 #   UF plots 
@@ -200,12 +270,12 @@ XglB<- select(Xgl, UF, Scenario, Biomass)
 
 require(scales)
 
-ufg<- ggplot(data=XglG, aes(x=Scenario, y=GrassArea, group=UF, color=UF))+
+ufg<- ggplot(data=XglG, aes(x=Scenario, y=GrassArea, group=UF, color=Scenario))+
   geom_point(size=10)+ 
   scale_shape_manual(values=c(1,16,21))+
   theme_bw()+
   scale_y_continuous(labels = comma)+
-  scale_x_discrete(labels=c("Base","1","2","3", "4")) +
+  scale_x_discrete(labels=c("Base","S1","S2","S3", "S4")) +
   theme(axis.text.x=element_text(size=22),
         axis.title.x=element_blank())+  #axis.text=element_text(size=)
   theme(axis.text.y=element_text(size=11, vjust=-0.35),
@@ -315,8 +385,21 @@ sg.col<-ggplot(data=subset(Sukh_All, UF %in%c("0.65")), aes(x=Year, y=GrassArea,
   labs(x="Year", y="Grassland area (ha)")
 sg.col
 
+#Sukh Grass Color BASELINE ONLY:
+sg.col.base<-ggplot(data=subset(Sukh_All, Scenario=="Baseline"), aes(x=Year, y=GrassArea, group=Scenario)) +
+  geom_line(size=1.2, color="#fb6a4a") +
+  guides(override.aes = list(size=12))+
+  scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Grassland area (ha)")
+sg.col.base
 
-
+clr.red="#fb6a4a"
 #LIVESTOCK POPULATION
 sl<- ggplot(data=subset(Sukh_All, UF %in%c("0.65")), aes(x=Year, y=LskPop, group=Scenario, shape=Scenario))+
   geom_line() + geom_point()+
@@ -345,6 +428,19 @@ sl.col<-ggplot(data=subset(Sukh_All, UF %in%c("0.65")), aes(x=Year, y=LskPop, gr
   labs(x="Year", y="Livestock Population EOY (s.u.)")
 sl.col
 
+#Lsk Color BASELINE ONLY:
+sl.col.base<-ggplot(data=subset(Sukh_All, Scenario=="Baseline"), aes(x=Year, y=LskPop, group=Scenario)) +
+  geom_line(size=1.2, color=clr.red ) +
+  guides(override.aes = list(size=12))+
+  scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Livestock Population EOY (s.u.)")
+sl.col.base
 
 #Biomass
 sr<- ggplot(data=subset(Sukh_All, UF %in%c("0.65")), aes(x=Year, y=Biomass, group=Scenario, shape=Scenario))+
@@ -374,6 +470,20 @@ sb.col<-ggplot(data=subset(Sukh_All, UF %in%c("0.65")), aes(x=Year, y=Biomass, g
   #theme(legend.position="none")+
   labs(x="Year", y="Remaining Biomass EOY (kg/ha)")
 sb.col
+
+#Biomass color BASELINE ONLY
+sb.col.base<-ggplot(data=subset(Sukh_All, Scenario=="Baseline"), aes(x=Year, y=Biomass, group=Scenario)) +
+  geom_line(size=1.2, color=clr.red) +
+  guides(override.aes = list(size=12))+
+  scale_shape_manual(values=c(1:11))+
+  theme_bw()+
+  theme(axis.text.x=element_text(size=11, angle=90))+
+  theme(axis.text.y=element_text(size=11, vjust=-0.35))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  scale_x_discrete(breaks=seq(1990,2050,5))+
+  #theme(legend.position="none")+
+  labs(x="Year", y="Remaining Biomass EOY (kg/ha)")
+sb.col.base
 
 setwd("./plots")
 ggsave(sr, file="Suk_rmb.png", height=5, width=8,dpi=600)
